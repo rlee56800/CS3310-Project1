0878dee98b8db28aeadda3d20c143ae170d8b223
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-10-15 02:23:21 -0700

    QS WORKS!!

--- quicksort.py
+++ quicksort.py
@@ -3,46 +3,43 @@ import time
 
 start_time = time.time()
 
-def partition(low, high):
-    #pivotpoint = low
-    i = low + 1
+def partition(low, high, arr):
     j = low
     pivotitem = arr[low]
-    print("q")
-    while i < high:
-        print("j")
+    for i in range(low+1, high):
         if arr[i] < pivotitem:
             j += 1
             arr[i], arr[j] = arr[j], arr[i]
         i += 1
     pivotpoint = j
     arr[low], arr[pivotpoint] = arr[pivotpoint], arr[low]
-    pivotpoint = high
+    return j
+    
 
-def quicksort(low, high):
-    #pivotpoint = low
+def quicksort(low, high, arr):
     if low < high:
-        partition(low, high)
-        quicksort(low, pivotpoint-1)
-        quicksort(pivotpoint+1, high) #idk
+        ppoint =  partition(low, high, arr)
+        quicksort(low, ppoint, arr)
+        quicksort(ppoint + 1, high, arr)
 
+'''
 # TESTING
 arr = [9, 2, 7, 8, 1]
 pivotpoint = 0
 print(arr)
-quicksort(0, len(arr)-1)
+quicksort(0, len(arr), arr)
 print(arr)
 '''
 arr=[]
 
-n = 50000000
+n = 5
 print(n) # to show which number is tested
 
 for i in range(n):
    arr.append(random.randint(0,100))
 
-#print(arr)
-quicksort(arr)
-#print(arr)
+print(arr)
+pivotpoint = 0
+quicksort(0, len(arr), arr)
+print(arr)
 print("Process finished --- %s seconds ---" % (time.time() - start_time))
-'''
